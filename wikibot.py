c6f4ae3b8defe856db6fd30e1aa17a46eb47c939
SUB0PT1MAL <92884665+SUB0PT1MAL@users.noreply.github.com>
2024-10-09 21:36:57 +0200

    Update wikibot.py

--- wikibot.py
+++ wikibot.py
@@ -46,7 +46,14 @@ async def search_wiki(wiki_key, query):
     last_request_time = asyncio.get_event_loop().time()
 
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'DNT': '1',  # Do Not Track
+    'Referer': 'https://google.com/',  # Sometimes adding a referer helps
     }
 
     async with aiohttp.ClientSession() as session:
